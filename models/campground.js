8073834202b180847db29459a5f0a74df0ce48e4
Saikat-Adhya <saikatadhya1@gmail.com>
2024-09-28 02:06:46 +0530

    Complete CRUD And their EJS Pages

--- models/campground.js
+++ models/campground.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-    title:String,
+    title:{
+        type:String,
+        required:[true, "Please enter Correct title"],
+    },
     price: String,
     description:String,
-    location:String
+    location:{
+        type:String,
+        required:[true, "Please enter City name"]
+    }
 });
 
 module.exports=mongoose.model('campground',CampgroundSchema);
